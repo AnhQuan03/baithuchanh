687f1b5d539a918b378f236869b21844f64e1d28
Nguyễn Anh Quân <Quanmilano1002@gmail.com>
2023-04-17 08:21:53 +0700

    del

--- BaiThucHanhLab3/BaiThucHanh21_03/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        HinhTron hinhTron = new HinhTron();
-        hinhTron.NhapBanKinh();
-        hinhTron.Tinh();
-        hinhTron.In();
-    }
-}
--- BaiThucHanhLab3/BaiThucHanh21_03/src/App1.java
+++
@@ -1,8 +0,0 @@
-public class App1 {
-    public static void main(String[] args) {
-        NhanVien nv = new NhanVien();
-        nv.Nhap();
-        nv.TinhLuong();
-        nv.In();
-    }
-}
